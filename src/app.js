1d8f18a38821ebd20396eb9f62f61fc5c92f0f74
joseterrera <joseterrera@yahoo.com>
2015-09-02 13:26:31 -0500

    an express app with jade

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
 'use strict';
 
-var express = require('express');
-var posts = require('./mock/posts.json');
+var express = require('express'),
+    posts = require('./mock/posts.json');
 
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates')
+
 app.get('/', function(req, res){
-	res.send("<h1>I love you</h1>");
+  res.render('index')
 });
 
-app.get('/blog', function(req, res){
-	res.send(posts);
+app.get('/blog/:title?', function(req, res){
+  var title = req.params.title;
+  if (title === undefined) {
+    res.status(503);
+    res.send("This page is under construction!");
+  } else {
+    var post = posts[title];
+    res.send(post);
+  }
 });
 
-app.listen(3000, function(){
-	console.log('The front end server is running on port 3000');
+app.listen(3000, function() {
+  console.log("The frontend server is running on port 3000!");
 });
